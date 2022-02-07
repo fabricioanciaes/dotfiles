2e16d512bb12751bb440ca839b2bf675c1ff2df7
fabricioanciaes <Fabricio4p93@gmail.com>
2022-02-07 03:34:22 -0300

    refazendo config do neovim

---
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -0,0 +1,10 @@
+local colorscheme = "tokyonight"
+
+vim.g.tokyonight_style = "night"
+vim.g.tokyonight_transparent = true
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  return
+end
